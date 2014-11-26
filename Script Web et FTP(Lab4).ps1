8e78a0953b29a06b7bf333984841eb6fd46cc5d6
xavierhd <xavierhd@hotmail.com>
2014-11-26 10:20:30 -0500

    Update script LAB4

--- Script Web et FTP(Lab4).ps1
+++ Script Web et FTP(Lab4).ps1
@@ -6,29 +6,33 @@
 New-NetIPAddress -IPAddress "10.57.154.113" -InterfaceAlias "Ext" -PrefixLength 16
 
 #Création de users
-net user "Jean Revien" "AAAaaa111" /ADD
-net user "Pierre Kiroule" "AAAaaa111" /ADD
-net user "Guy Tard" "AAAaaa111" /ADD
+net user "Jean" "AAAaaa111" /ADD
+net user "Pierre" "AAAaaa111" /ADD
+net user "Guy" "AAAaaa111" /ADD
 
 #Check la création des users
 net localgroup "Utilisateurs"
 
 #Crée les dossiers
-New-Item -Name "_WEB" -Path "c:\" -type directory
+New-Item -Name "_WEB\Corpo" -Path "c:\" -type directory
+New-Item -Name "Vendeur" -Path "c:\_WEB\" -type directory
+New-Item -Name "Achat" -Path "c:\_WEB\" -type directory
+New-Item -Name "ww1" -Path "c:\_WEB\" -type directory
+New-Item -Name "ww2" -Path "c:\_WEB\" -type directory
 New-Item -Name "_WEBA1" -Path "c:\" -type directory
 New-Item -Name "_WEBA2" -Path "c:\" -type directory
 
 New-Item -Name "_FTP\Public" -Path "c:\" -type directory
 New-Item -Name "doc.txt" -Path "c:\_FTP\Public\" -type file
 
-New-Item -Name "_FTP\Intranet\Pierre" -Path "c:\" -type directory
-New-Item -Name "_FTP\Intranet\Jean" -Path "c:\" -type directory
-New-Item -Name "_FTP\Intranet\Default" -Path "c:\" -type directory
+New-Item -Name "Intranet\Pierre" -Path "c:\_FTP\" -type directory
+New-Item -Name "Jean" -Path "c:\_FTP\Intranet\" -type directory
+New-Item -Name "Default" -Path "c:\_FTP\Intranet\" -type directory
 
 New-Item -Name "_FTP\Perso\LocalUser\Administrateur" -Path "c:\" -type directory
-New-Item -Name "_FTP\Perso\LocalUser\Guy" -Path "c:\" -type directory
-New-Item -Name "_FTP\Perso\LocalUser\Jean" -Path "c:\" -type directory
-New-Item -Name "_FTP\Perso\LocalUser\Public" -Path "c:\" -type directory
+New-Item -Name "Guy" -Path "c:\_FTP\Perso\LocalUser\" -type directory
+New-Item -Name "Jean" -Path "c:\_FTP\Perso\LocalUser\" -type directory
+New-Item -Name "Public" -Path "c:\_FTP\Perso\LocalUser\" -type directory
 
 #Remove NTFS Permissions
 ICACLS "c:\_FTP" /inheritance:r
@@ -43,7 +47,6 @@ ICACLS "c:\_FTP\Intranet" /grant "Utilisateurs:RX"
 
 ICACLS "c:\_FTP\Intranet\Jean" /grant "Jean Revien:M"
 ICACLS "c:\_FTP\Intranet\Pierre" /grant "Pierre Kiroule:M"
-ICACLS "c:\_FTP\Intranet\Default" /grant "Default:M"
 
 ICACLS "c:\_FTP\Perso" /grant "Administrateurs:(OI)(CI)F"
 ICACLS "c:\_FTP\Perso" /grant "Système:(OI)(CI)F"
@@ -51,4 +54,3 @@ ICACLS "c:\_FTP\Perso" /grant "Utilisateurs:RX"
 
 ICACLS "c:\_FTP\Perso\LocalUser\Jean" /grant "Jean Revien:M"
 ICACLS "c:\_FTP\Perso\LocalUser\Guy" /grant "Guy Tard:M"
-ICACLS "c:\_FTP\Perso\LocalUser\Public" /grant "Default:R"
