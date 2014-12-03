2d9f174ae61ff6bd11c75fe5187be4798b01ef62
xavierhd <xavierhd@hotmail.com>
2014-12-03 10:40:09 -0500

    rewipage

--- R5s2.ps1
+++ R5s2.ps1
@@ -8,4 +8,5 @@ if((Get-WindowsFeature -Name DNS).Installed -eq $false)
     Install-WindowsFeature DNS –IncludeManagementTools
 }
 
-Add-DnsServerPrimaryZone -Name vendeur.corpo.com21 -ZoneFile corpo.com21.dns -DynamicUpdate None
+Add-DnsServerPrimaryZone -Name achat.corpo.com21 -ZoneFile corpo.com21.dns -DynamicUpdate None
+Add-DnsServerResourceRecordA -Name www -ZoneName corpo.com21 -IPv4Address 10.57.154.111
--- R5sr.ps1
+++ R5sr.ps1
@@ -9,4 +9,9 @@ if((Get-WindowsFeature -Name DNS).Installed -eq $false)
 }
 
 Add-DnsServerPrimaryZone -Name corpo.com21 -ZoneFile corpo.com21.dns -DynamicUpdate None
-Add-DnsServerSecondaryZone -Name 
+Add-DnsServerResourceRecordA -Name www -ZoneName corpo.com21 -IPv4Address 10.57.154.111
+Add-DnsServerResourceRecordA -Name www.vendeur -ZoneName corpo.com21 -IPv4Address 10.57.154.111
+Add-DnsServerResourceRecordA -Name intranet -ZoneName vendeur.corpo.com21 -IPv4Address 10.57.154.111
+Add-DnsServerResourceRecordA -Name doc -ZoneName corpo.com21 -IPv4Address 10.57.154.111
+
+Add-DnsServerZoneDelegation -ChildZoneName achat -IPAddress 10.57.154.112 -Name corpo.com21 -NameServer 529S2V21
